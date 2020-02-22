b3bc19a7e10f60dcc4913f596b07078b421d10b4
wflixu <wflixu@yeah.net>
2020-02-22 16:55:10 +0800

    test

--- lib/config/app_config.dart
+++
@@ -1,8 +0,0 @@
-
-
-class AppConfig {
-  static String appId = 'awgkVY8XvUY5oWtvmzRH6ylj-gzGzoHsz';
-  static  String appKey = 'GJnJ1a8KVnaVLquMKj6uSllD';
-  static String server = 'https://awgkvy8x.lc-cn-n1-shared.com';
-
-}
